c8e9402307dcf47e29597fbcdcf25952fdd879e1
awsbasiya <awsbasiya@localhost>
2023-04-06 17:16:14 -0400

    Class5 commit

---
+++ src/Demo5/Class5.java
@@ -0,0 +1,16 @@
+package Demo5;
+
+public class Class5 {
+	
+public static void main (String [] args) {
+		
+		System.out.println("Class5........");
+		
+		System.out.println("Git Class5.....");
+		
+		
+	}
+
+
+
+}
